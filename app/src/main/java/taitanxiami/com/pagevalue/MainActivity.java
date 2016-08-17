14c0adcde208ad6628bdb93b3983641179ef3bf6
taitanxiami <taitanxiami@gmail.com>
2016-08-17 22:56:19 +0800

    完成页面见传值

--- app/src/main/java/taitanxiami/com/pagevalue/MainActivity.java
+++ app/src/main/java/taitanxiami/com/pagevalue/MainActivity.java
@@ -1,13 +1,15 @@
 package taitanxiami.com.pagevalue;
 
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-    public final static String EXTRA_MESSAGE = "com.mycompany.myfirstapp.MESSAGE";
+    public final static String EXTRA_MESSAGE = "taitanxiami.com.pagevalue.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,10 +19,23 @@ public class MainActivity extends AppCompatActivity {
     public void sendMessage(View view) {
 
     // Do something in response to button
-
-        Intent intent = new Intent(this,DisplayMessageActivity.class);
         EditText editText = (EditText)findViewById(R.id.text_id);
         String message = editText.getText().toString();
+        if (message == null || message.length() == 0) {
+
+            Log.i("tag","不能传空值");
+
+//            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+//            builder.setMessage(R.string.dialog_message)
+//                    .setTitle(R.string.dialog_title);
+//
+//// 3. Get the AlertDialog from create()
+//            AlertDialog dialog = builder.create();
+            return;
+        }
+        Intent intent = new Intent(this,DisplayMessageActivity.class);
+
         intent.putExtra(EXTRA_MESSAGE,message);
+        startActivity(intent);
     }
 }
